44904684c31b76f3763c923732d8cc44571f727a
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-09-06 10:43:45 -0300

    atualização header
    6-9-23

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const body = document.body,
+      themeToggle = document.querySelectorAll('.theme-toggle'),
+      lightMode = document.querySelector('.light-mode-icon'),
+      DarkMode = document.querySelector('.dark-mode-icon');
+
+//adicionar o evento de clique para alternar o tema
+themeToggle.forEach(button => {
+  button.addEventListener('click', toggleDarkTheme);
+});
+
+//função para alternar entre os temas
+function toggleDarkTheme() {
+  body.classList.toggle('dark');
+
+  //salvar o estado do tema no armazenamento local(localstorage)
+  if(body.classList.contains('dark')){
+    localStorage.setItem('dark-theme','true');
+    DarkMode.style.display = 'block';
+    lightMode.style.display = 'none';
+  } else {
+    localStorage.removeItem('dark-theme');
+    DarkMode.style.display = 'none';
+    lightMode.style.display = 'block';
+  }
+}
+
+//verificar se o tema escuro está ativado no carregamento da página
+if(localStorage.getItem('dark-theme')) {
+  body.classList.add('dark');
+  DarkMode.style.display = 'block';
+  lightMode.style.display = 'none';
+} else {
+  DarkMode.style.display = 'none';
+  lightMode.style.display = 'block';
+}
+
+
+
+//dropdown
+const buttonIdioma = document.querySelector('.js-select'),
+      dropdown = document.querySelector('.js-menu');
+
+buttonIdioma.addEventListener('click', () => {
+    dropdown.classList.toggle('hidden');
+
+    document.addEventListener('click', (event) => {
+      if (!dropdown.contains(event.target) && !buttonIdioma.contains(event.target)) {
+        dropdown.classList.add('hidden');
+      }
+    });  
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    colors:{
+      'fundo':'#202124',
+      'gray':'#737373',
+      'light-green':'#00C4C3',
+      'medium-green':'#007877',
+      'normal-green':'#006363',
+      'black':'#0E0E0E',
+      'dark-green':'#002726',
+      'white':'#fff',
+      'light':'#f5f5f5'
+    },
+    extend: {
+      fontFamily: {
+        koulen: "'Koulen', cursive",
+        roboto: "'Roboto Slab', serif",
+        bebas: "'Bebas Neue', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
